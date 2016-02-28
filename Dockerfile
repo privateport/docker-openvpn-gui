03f024165aef5c1d17f6b08122db1165455e94ce
SneakyScampi <admin@privateport.io>
2016-02-28 12:52:59 +0000

    Use ENV variables for hostame

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,11 @@ RUN apk update && apk upgrade \
 		&& apk add alpine-sdk autoconf automake libtool gettext bison flex ragel boost-dev | tee /tmp/install.txt \
 		&& cd /root/openvpn-webca \
 		&& npm install --unsafe-perm \ 
-		&& apk del `grep 'Installing' /tmp/install.txt | awk {'print $3'} | xargs echo` && rm -rf /tmp/install.txt \
+		&& apk del `grep 'Installing' /tmp/install.txt | awk {'print $3'} | xargs echo` && rm -rf /tmp/install.txt 
 
 COPY config.sh /opt/
 COPY start.sh /opt/
 
 EXPOSE 3000/tcp
-CMD ["/opt/start.sh"]
+ENTRYPOINT ["/opt/start.sh"]
+#CMD [""]
--- start.sh
+++ start.sh
@@ -1,3 +1,47 @@
 #!/bin/bash
+
+function print_help {
+cat <<EOF
+Usage:
+        -h | --help                     Print this help
+        -h | --pphostname )     Privateport.io HOSTNAME
+        -e | --ppeasyname )     Privateport.io EASYNAME
+_______________________________________________
+by privateport.io
+EOF
+}
+
+OPTS=`getopt -o h:e: --long hostname:,easyname: -n 'parse-options' -- "$@"`
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+echo #OPTS
+eval set -- "$OPTS"
+while true; do
+  case "$1" in
+        -h | --help )           print_help; exit 0; shift ;;
+        -h | --pphostname )     PPHOSTNAME="$2"; shift; shift ;;
+        -e | --ppeasyname )     PPEASYNAME="$2"; shift; shift ;;
+        -x | --debug )          DEBUG=true; shift ;;
+        -- ) shift; break ;;
+        * ) break ;;
+  esac
+done
+
+echo $PPHOSTNAME > /etc/pphostname
+echo $PPEASYNAME > /etc/ppeasyname
+
+if [ -n "$DEBUG" ]; then
+        /bin/bash
+        exit 0
+fi
+
+cd /root/openvpn-webca && git pull
+git clone https://github.com/privateport/openssl-utils.git /tmp/openssl-utils \
+                && /tmp/openssl-utils/install.sh \
+                && rm -rf /tmp/openssl-utils \
+        && git clone https://github.com/privateport/openvpn-utils.git /tmp/openvpn-utils \
+                && /tmp/openvpn-utils/install.sh \
+                && rm -rf /tmp/openvpn-utils
+
 cd /root/openvpn-webca
 npm start
